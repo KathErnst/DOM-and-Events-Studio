cf47d62c18b60598f7ded322152b16fd8a77effa
Katherine Ernsting <katherineernsting@Katherines-MacBook-Air.local>
2023-02-28 21:28:36 -0600

    Added steps 2d, 3 and 4

--- scripts.js
+++ scripts.js
@@ -1,16 +1,25 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener("load", function () {
-    const takeOff = document.getElementById("takeoff");
+    const takeOffButton = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
     const shuttleBackground = document.getElementById("shuttleBackground");
-    takeOff.addEventListener("click", function() {
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const landButton = document.getElementById("landing");
+
+    takeOffButton.addEventListener("click", function() {
         let ready = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (ready) {
             flightStatus.innerHTML = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML += 10000;
         }
+    });
+    landButton.addEventListener("click", function () {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = 0;
+    });
 
-
-});
 })
