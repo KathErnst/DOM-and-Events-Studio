6841575cba77edc7d99d94b8cf6e9f180997087d
Katherine Ernsting <katherineernsting@Katherines-MacBook-Air.local>
2023-02-28 20:25:01 -0600

    Completed Steps 1 and 2a-c

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function () {
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    takeOff.addEventListener("click", function() {
+        let ready = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (ready) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shuttleBackground.style.backgroundColor = "blue";
+        }
+
+
+});
+})
